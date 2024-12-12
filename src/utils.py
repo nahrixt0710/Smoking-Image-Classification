f9e7294ad7fea8b0cceb2cd5ff48cdf08aaa169e
gbaonr <gbaon.r@gmail.com>
2024-12-13 00:32:59 +0700

    knn_demo

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from skimage import feature, io, color
 
 BIN_SIZE = 16
 
+
 def extract_color_histogram(image, bins=(BIN_SIZE, BIN_SIZE, BIN_SIZE)):
     hist = []
     for i in range(3):
@@ -13,14 +14,16 @@ def extract_color_histogram(image, bins=(BIN_SIZE, BIN_SIZE, BIN_SIZE)):
         hist.extend(channel_hist)
     return np.array(hist)
 
+
 def preprocess(image):
 
-    image = cv2.resize(image, (128, 128)) 
+    image = cv2.resize(image, (128, 128))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     feature = extract_color_histogram(image)
 
     return feature
+
 
 def load_data_from_folder(folder_path):
     data = []
@@ -51,3 +54,66 @@ def predict(image, model):
 
     return "Not smoking" if prediction[0] == 0 else "Smoking"
 
+
+# utils to optimize bin size and image size
+
+
+def load_raw_data(folder_path):
+    data = []
+    labels = []
+
+    for img_name in os.listdir(folder_path):
+
+        img_path = os.path.join(folder_path, img_name)
+        image = cv2.imread(img_path)
+
+        if image is not None:
+            data.append(image)
+            if img_name.lower().startswith("notsmoking"):
+                labels.append(0)
+            elif img_name.lower().startswith("smoking"):
+                labels.append(1)
+        else:
+            print(f"Can't load image: {img_name}")
+
+    return np.array(data), np.array(labels)
+
+
+def calc_hist(image, bin):
+    bins = (bin, bin, bin)
+    hist = []
+    for i in range(3):
+        channel_hist = cv2.calcHist([image], [i], None, [bins[i]], [0, 256])
+        channel_hist = cv2.normalize(channel_hist, channel_hist).flatten()
+        hist.extend(channel_hist)
+    return np.array(hist)
+
+
+class CustomFeatureExtractor:
+    def __init__(self, bins=16, image_size=128):
+        self.bins = bins
+        self.image_size = image_size
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        transformed = []
+        for image in X:
+            if self.image_size:
+                image = cv2.resize(image, (self.image_size, self.image_size))
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+            hist = calc_hist(image, self.bins)
+            transformed.append(hist)
+        return np.array(transformed)
+
+    def set_params(self, **params):
+        # Cập nhật tham số
+        for key, value in params.items():
+            setattr(self, key, value)
+        return self
+
+    def get_params(self, deep=True):
+        # Lấy tham số
+        return {"bins": self.bins, "image_size": self.image_size}
