f7a942f540d4f74a9c588e471c821e8715187970
nahrixt0710 <164285509+nahrixt0710@users.noreply.github.com>
2024-12-13 18:21:09 +0700

    add split.py and update SVM gridsearch

---
+++ split_data.py
@@ -0,0 +1,26 @@
+import os
+from sklearn.model_selection import train_test_split
+import shutil
+# Đường dẫn đến thư mục ảnh gốc
+source_folder = './data/images'
+train_folder = './data/train'
+test_folder = './data/test'
+
+# Tạo các thư mục nếu chưa tồn tại
+os.makedirs(train_folder, exist_ok=True)
+os.makedirs(test_folder, exist_ok=True)
+
+# Lấy danh sách các tệp ảnh
+image_files = [f for f in os.listdir(source_folder) if os.path.isfile(os.path.join(source_folder, f))]
+
+# Chia dữ liệu thành train và test
+train_files, test_files = train_test_split(image_files, test_size=0.2, random_state=42)
+
+# Di chuyển các tệp vào thư mục tương ứng
+for file in train_files:
+    shutil.copy2(os.path.join(source_folder, file), os.path.join(train_folder, file))
+
+for file in test_files:
+    shutil.copy2(os.path.join(source_folder, file), os.path.join(test_folder, file))
+
+print("Done splitting images!")
