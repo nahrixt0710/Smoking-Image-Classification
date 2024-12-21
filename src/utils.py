5fe1b8ea2c016f75a684a0a89c3ec68c40e3fba8
nahrixt0710 <164285509+nahrixt0710@users.noreply.github.com>
2024-12-21 20:51:01 +0700

    add hsv code

--- src/utils.py
+++ src/utils.py
@@ -14,20 +14,42 @@ def extract_color_histogram(image, bins=(BIN_SIZE, BIN_SIZE, BIN_SIZE)):
         hist.extend(channel_hist)
     return np.array(hist)
 
+import cv2
+import numpy as np
+
+def extract_color_histogram_hsv(image, bins_h=180, bins_s=256, bins_v=256):
+    
+    # Tính histogram cho từng kênh HSV
+    hist_hue = cv2.calcHist([image], [0], None, [bins_h], [0, 180])
+    hist_saturation = cv2.calcHist([image], [1], None, [bins_s], [0, 256])
+    hist_value = cv2.calcHist([image], [2], None, [bins_v], [0, 256])
+    
+    # Chuẩn hóa histogram về khoảng [0, 1]
+    hist_hue = cv2.normalize(hist_hue, hist_hue, 0, 1, cv2.NORM_MINMAX)
+    hist_saturation = cv2.normalize(hist_saturation, hist_saturation, 0, 1, cv2.NORM_MINMAX)
+    hist_value = cv2.normalize(hist_value, hist_value, 0, 1, cv2.NORM_MINMAX)
+    
+    # Ghép các histogram lại thành một vector duy nhất
+    hist_vector = np.concatenate((hist_hue.flatten(),
+                                   hist_saturation.flatten(),
+                                   hist_value.flatten()))
+    
+    return hist_vector
 
 # not used
-def preprocess(image):
+def preprocess_hsv(image, img_size=250, bins_h=180, bins_s=256, bins_v=256):
 
-    image = cv2.resize(image, (64, 64))
+    image = cv2.resize(image, (img_size, img_size))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    feature = extract_color_histogram(image)
+    # feature = extract_color_histogram(image)
+    feature = extract_color_histogram_hsv(image, bins_h, bins_s, bins_v)
 
     return feature
 
 
 # using
-def preprocess2(image, img_size=256, bin=16):
+def preprocess2(image, img_size=250, bin=16):
 
     image = cv2.resize(image, (img_size, img_size))
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
